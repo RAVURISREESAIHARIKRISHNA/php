aacf398828f70458fc437de1fde7b6fc95e8240f
RAVURI SREE SAI HARI KRISHNA <RAVURISREESAIHARIKRISHNA@users.noreply.github.com>
2017-08-03 22:33:08 +0530

    Add files via upload

---
+++ Variable2.php
@@ -0,0 +1,10 @@
+<?php
+    //Creating a variable
+    $x = 1;
+    echo "My variable is :<br/>";
+    echo $x;
+    echo"<br/> Now I'm gonna store a String";
+    $x = "Hello";
+    echo "<br/>My new Value is :<br/>";
+    echo $x;
+?>
